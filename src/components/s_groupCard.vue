577ef4e85adb2cf0d518f2585b14482e46d5db79
plq071900 <574716272@qq.com>
2018-09-13 17:56:46 +0800

    分工不分

---
+++ src/components/s_groupCard.vue
@@ -0,0 +1,246 @@
+<template>
+  <div class="order-shop-card">
+    <div class="item-card">
+      <!-- <i class="select" v-if="edit" @click="select" :class="[check ? 'active' : 'close']"></i> -->
+      <div class="img" :style="{background: 'url(' + shop.images + ')'}"></div>
+      <div class="desc">
+        <p class="title">{{shop.name}}</p>
+        <p class="tips">
+          <span>档口:{{shop.stallInfo1 + '-' + shop.stallInfo2 +'-'+ shop.stallInfo3}}</span>
+          <button class="group">{{shop.ping.num}}人成团</button>
+        </p>
+        <p class="sellnum">货期:{{shop.delivery}}丨销量:{{shop.sellCount}}</p>
+        <p class="collage_price">拼团价: {{groupPrice ? '￥' + groupPrice : '未设置'}}</p>
+        <p class="price">
+          <span class="countPrice">售价:￥{{shop.sellPrice}}</span>
+          <span class="sell">&nbsp;&nbsp;&nbsp;利润:￥{{shop.profit}}</span>
+          <i class="more-icon" @click="toOpen('visible4')"></i>
+        </p>
+      </div>
+    </div>
+    <i-modal :visible="visible2" @ok="comfirmDel('visible2')" @cancel="toClose('visible2')">
+      <div class="m_tips">确定要{{actions4[1].name}}该商品？</div>
+    </i-modal>
+    <i-modal title="请选择操作" :visible="visible4" :actions="actions4" :action-mode="vertical" @iclick="handleClick4"></i-modal>
+  </div>
+</template>
+<script>
+import noImage from '@/assets/img/shopMgr/noImage.png'
+import mixin from '@/mixin'
+export default {
+  mixins: [mixin],
+  props: {
+    //编辑
+    edit: {
+      type: Boolean,
+      default: false
+    },
+    //添加商品
+    shop: {
+      type: Object,
+    },
+    act: {
+      type: String,
+      default: '下架'
+    },
+    // check: {
+    //   type: Boolean,
+    //   default: false
+    // }
+  },
+  watch: {
+    edit: (newV,old)=>{
+      // if(!newV)
+      this.edit = !this.edit
+      console.log(this.edit)
+    }
+  },
+  data(){
+    return {
+      check: false,
+      visible4: false,
+      visible2: false,
+      // vertical: '',
+      actions4: [
+        { name: "编辑" },
+        { name: "下架" },
+        { name: "分组" },
+        { name: "取消" }
+      ],
+    }
+  },
+  computed: {
+    // getImg1(){
+    //   if(this.shop.images){
+    //     return this.shop.images.split(',')[0]
+    //   } else {
+    //     return noImage
+    //   }
+    // },
+    // checkState(){
+    //   return this.check
+    // }
+  },
+  methods: {
+    //点击选中按钮
+    select(){
+      //通知父组件 选中或取消
+      if(this.check) {
+        this.check = false
+        // this.$emit('switchSel', this.shop.id, false, this.shop)
+      }else {
+        this.check = true
+        // this.$emit('switchSel', this.shop.id, true, this.shop)
+      }
+
+    },
+    // selectAll(bool){
+    //   this.check = bool
+    // },
+    handleClick4({ mp: { detail } }) {
+      const index = detail.index
+      this.visible4 = false
+      if(index === 0) {
+        //edit
+        console.log(this.shop);
+        this.toRoute('home/shopMgr/collage/collageMsg', {pingInfo: JSON.stringify(this.shop.ping), isEdit: true})
+        // this.$emit('openEdit')
+      } else if(index === 1) {
+        //下架 或上架
+        this.visible2 = true
+      } else if (index === 2) {
+        //group
+        this.toRoute('home/shopMgr/classify',{groupIds: this.shop.groupIds,goodsIds: this.shop.id })
+      }
+      else if (index === 2) {
+        //group
+        this.toClose('visible4')
+      }
+    },
+
+    async comfirmDel(name){
+      this[name] = false
+      let state = 0
+      if(this.act != '下架') state = 1
+      await this.$API.switchShop({
+        goodsIds: this.shop.id,
+        state: state
+      })
+      this.show = false
+      this.$success(this.act + '成功！')
+    },
+    // resetCheck(){
+    //   this.check = false
+    // }
+  },
+  mounted() {
+    // this.check = false
+    this.actions4[1].name = this.act
+    console.log(this.shopList)
+  },
+
+}
+</script>
+<style lang="sass" scoped>
+@import '~@/assets/css/mixin'
+// .m_tips
+//   color: #000
+//   font-size: 32px
+//   padding: 40px 0
+// .select-box
+//   width: 60px
+//   position: relative
+// .select
+//   +select()
+// .active
+//   +select-active
+.order-shop-card
+  width: 100%
+  min-height: 100%
+  .item-card
+    display: flex
+    align-items: center
+    text-align: left
+    padding: 32px 16px 28px 24px
+    // +border(1px,bottom)
+    background: #fff
+    margin-bottom: 10px
+    i.select
+      display: inline-block
+      width: 40px
+      height: 40px
+      border-radius: 50%
+      margin: auto 20px
+      background-position: center
+      background-repeat: no-repeat
+      background-size: 100% 100%
+    i.active
+      background-image: url("~@/assets/img/storeMgr/select.png")
+    i.close
+      background-image: url("~@/assets/img/home/select.png")
+    .img
+      +icon(240px)
+      border-radius: 12px
+      margin-right: 20px
+    .desc
+      flex: 1
+      display: flex
+      flex-wrap: wrap
+      justify-content: center
+      overflow: hidden
+      position: relative
+      .title
+        width: 100%
+        +moreLine(2)
+        font-size: 30px
+      .tips, .sellnum
+        width: 100%
+        padding-top: 10px
+        color: #999
+        font-size: 26px
+        +singleFile
+      .tips
+        display: flex
+        align-items: center
+        justify-content: space-between
+        span
+          flex: 1
+          +singleFile
+        button.group
+          padding: 8px 15px
+          color: #EE7527
+          font-size: 26px
+          border: 2px solid #EE7527
+          border-radius: 20px
+          margin-right: 35px
+          +singleFile
+      .collage_price
+        width: 100%
+        padding: 5px
+        color: #333
+        font-size: 28px
+        +singleFile
+      .price
+        width: 100%
+        display: flex
+        flex-wrap: wrap
+        align-items: center
+        .countPrice
+          flex: 1
+          color: #FF0000
+          font-size: 30px
+          text-align: left
+          +singleFile
+        .sell
+          flex: 1
+          color: #333
+          font-size: 28px
+          text-align: left
+          +singleFile
+        i.more-icon
+          display: inline-block
+          width: 40px
+          height: 40px
+          background: url("~@/assets/img/shopMgr/more.png") no-repeat center
+          background-size: 100% 100%
+</style>
---
+++ src/pages/home/shopMgr/shopListDiscount.vue
@@ -0,0 +1,301 @@
+<template>
+  <div class="home">
+    <!-- 商品选择列表 -->
+    <div class="sort-select">
+      <div class="top-nav">
+        <ul>
+          <li :class="[tag === 1 && 'nav-active']" @click="handleTag(1)">综合</li>
+          <li :class="[tag === 2 && 'nav-active']" @click="handleTag(2)">销量<div class="sort-box"><i class="sort-top" :class="asceSale && 'sort-active'"></i><i :class="!asceSale && 'sort-active'" class="sort-bottom"></i></div></li>
+          <li :class="[tag === 4 && 'nav-active']" @click="handleTag(4)">价格<div class="sort-box"><i class="sort-top" :class="ascePrice && 'sort-active'"></i><i :class="!ascePrice  && 'sort-active'" class="sort-bottom"></i></div></li>
+          <li :class="[tag === 5 && 'nav-active']" @click="handleTag(5)">筛选<div class="sort-box"><i class="option-icon"></i></div></li>
+        </ul>
+      </div>
+      <scroll-view scroll-y lower-threshold='80' style="height: 82%;" @scrolltolower="lower"  >
+      <div class="scroll-box">
+        <div class="box">
+          <p v-for="(shop,index) in shopList" :key="index" >
+            <scard ref="scard" :edit="edit" :key="index" :shop="shop" @switchSel="switchSel" @setGroupPrice="setGroupPrice" />
+          </p>
+        </div>
+        <div class="loading" v-if="canLoad">
+          <div v-if="showLoad"><loading  /></div>
+        </div>
+      </div>
+      <i-drawer mode="right" :visible="showRight1" @close="toggleRight1">
+        <selsearch @comSearch="comSearch" />
+      </i-drawer>
+      </scroll-view>
+    </div>
+    <div class="footer">
+      <p class="save" @click="confirm">确定{{groupNum}}</p>
+      <!-- <p class="save" @click="toRoute('home/shopMgr/collage/collageMsg')">确定{{groupNum}}</p> -->
+    </div>
+    <i-message id="message" />
+
+  </div>
+</template>
+<script>
+import wx from "wx"
+import scard from '@/components/s_listCardDiscount'
+import loading from "@/commond/loading";
+import mixin from '@/mixin'
+import selsearch from '@/components/selectSearch'
+import { mapState } from 'vuex'
+export default {
+  mixins: [mixin],
+  components: {
+    scard,
+    loading,
+    selsearch
+  },
+  computed: {
+    groupNum(){
+      return this.selIds.length > 0 ? '('+ this.selIds.length +')' : ''
+      // return this.$SETGROUNPPRICE.length > 0 ? ``
+    },
+    ...mapState(["discountSelectList"])
+  },
+  watch: {
+    edit(currentValue, oldValue) {
+      this.edit = !this.edit
+    }
+  },
+  data() {
+    return {
+      showUp: false,
+      asceSale: true,
+      ascePrice: true,
+      tag: 1,
+      showRight1: false,
+      edit: true,
+      allCheck: false,
+      //loading
+      shopNum: 0,
+      shopList: [],//商品列表
+      showLoad: false,
+      canLoad: true,
+      pageSize: 20,
+      type: 0,
+      selIds: [],//选择的ids
+      state: 1,
+      groupPriceData: [],//选择的id和拼团价
+    };
+  },
+  methods: {
+    confirm(){
+      console.log(this.discountSelectList);
+      const goodsId = []
+      this.discountSelectList.forEach((item, index) => {
+        goodsId[index] = item.id
+      })
+      const disPrice = []
+      this.discountSelectList.forEach((item, index) => {
+        disPrice[index] = item.groupPrice
+      })
+      this.$API.addDisGoods({
+        goodsIds: goodsId.toString(),
+        disPrice: disPrice.toString(),
+      }).then(response => {
+        if(response.code == 0) {
+          this.$router.back(-1);
+        }
+      })
+    },
+    async handleTag(tag){
+      this.tag = tag
+      var type = 0 ;
+      //refresh init
+      this.shopNum = 0
+      this.canLoad = true
+      this.showLoad = false
+      if(tag === 2){
+        //对销量sort
+        this.asceSale = !this.asceSale
+        type = this.asceSale ? 2 : 3
+      }
+      if(tag === 4) {
+        this.ascePrice = !this.ascePrice
+        type = this.ascePrice ? 4 : 5
+      }
+      if(tag === 5) {
+        this.toggleRight1()
+        return
+      }
+      this.type = type
+      const listData = await this.getNextPage({
+        ob: type,
+        state: this.state
+      })
+      this.shopList = listData.data.list
+      if(listData.data.list.length < this.pageSize) {
+        this.canLoad = false
+      }
+    },
+    toggleRight1 () {
+      this.showRight1 = !this.showRight1
+    },
+    searchShop(params){
+      params.pageSize = this.pageSize
+      return this.$API.s_getCanGroup(params)
+      // return this.$API.s_getCanGroup(params)
+    },
+    getNextPage(params) {
+      this.shopNum++;
+      //追加条件，根据tag追加
+      if(this.tag == 5){
+        params = Object.assign(params, this.selParam)
+      }
+
+      params.pageNumber = this.shopNum;
+      return this.searchShop(params)
+      // return this.$API.getAdr(obj);
+    },
+    async lower(e) {
+      // console.log(e);
+      if (!this.canLoad) return;
+      if (this.showLoad) return;
+      this.showLoad = true;
+      const listData = await this.getNextPage({ob: this.type,state: this.state});
+      setTimeout(() => {
+        if (listData.data.list.length < this.pageSize) {
+          this.canLoad = false;
+        }
+        this.shopList = this.shopList.concat(listData.data.list);
+        this.showLoad = false;
+      }, 2000);
+    },
+    async comSearch(searchParams){
+      //searchParams 筛选的查询参数
+      this.toggleRight1()
+      this.selParam = searchParams
+      const listData = await this.getNextPage({ob: 0,state: this.state})
+      this.shopList = listData.data.list;
+      if (listData.data.list.length < this.pageSize) {
+        this.canLoad = false;
+      }
+
+    },
+    // 根据选中状态处理ids: GoodsId, 拼团价, 是否已勾选
+    switchSel(id, groupPrice, bool){
+      if(bool){
+        if(this.selIds && this.selIds.some(item => item == id)) {
+          const start = this.selIds.indexOf(id)
+          this.selIds[start] = id;
+          this.groupPriceData[start] = groupPrice;
+          this.$store.commit('SETDISOUNTPRICE', start, {id, groupPrice});
+        }else {
+          this.selIds.push(id);
+          this.groupPriceData.push(groupPrice)
+          this.$store.commit('PUSTDISOUNTPRICE', {id, groupPrice});
+        }
+        // console.log(this.groupPriceData);
+      }else {
+        const start = this.selIds.indexOf(id)
+        this.selIds.splice(start, 1)
+        this.groupPriceData.splice(start, 1)
+        // console.log(this.groupPriceData);
+        this.$store.commit('SPLICEDISOUNTPRICE', start);
+      }
+      // this.$store.commit('SETGROUNPPRICE', [this.selIds, this.groupPriceData])
+      console.log(this.discountSelectList);
+    },
+    // 设置拼团id和价格数组
+    // obj: {id: this.shop.id, price: this.groupPrice}
+    setGroupPrice(obj){
+      for(var i=0,l; l=this.groupPriceData[i++];){
+        if(l.id == obj.id) return l.price = obj.price
+      }
+      this.groupPriceData.push(obj)
+      console.log(this.groupPriceData);
+    }
+
+
+  },
+  async mounted() {
+    console.log(this.shopList)
+    this.shopNum = 0;
+    const listData = await this.getNextPage({ob: 0,state: this.state});
+    this.shopList = listData.data.list;
+    if (listData.data.list.length < this.pageSize) {
+      this.canLoad = false;
+    }
+    console.log(this.shopList)
+
+  },
+
+};
+</script>
+<style lang="sass" scoped>
+@import '~@/assets/css/mixin'
+.footer
+  position: absolute
+  bottom: 0
+  left: 0
+  right: 0
+  height: 98px
+  line-height: 98px
+  color: #fff
+  background: #F67C2F
+  text-align: center
+  .save
+
+.loading
+  height: 70px
+
+
+
+
+.box
+  padding: 2% 0 50px 0%
+.home
+  height: 100%
+
+.scroll-box
+  // padding: 10px 0
+  height: 800px
+  // overflow: auto
+  p
+    margin: 5px 0
+.option-icon
+  +bg-img('shopMgr/option.png')
+  +icon(32px)
+.sort-active
+  &:after
+    border-color: #EE7527!important
+.nav-active
+  color: #EE7527!important
+.sort-select
+  height: 100%
+.top-nav
+  // left: 0
+  // right: 0
+  // top: 180px
+  background: #fff
+  // z-index: 9999
+  text-align: center
+  ul
+    display: flex
+    font-size: 26px
+    height: 92px
+    line-height: 92px
+    li
+      flex: 1
+      .sort-box
+        display: inline
+        position: relative
+        padding-left: 10px
+        .sort-bottom
+          +goback(1px)
+          position: absolute
+          top: -23px
+          &:after
+            transform: rotate(-45deg)
+            border-color: #999
+        .sort-top
+          +goback(1px)
+          position: absolute
+          bottom: -23px
+          &:after
+            transform: rotate(-225deg)
+            border-color: #999
+</style>
